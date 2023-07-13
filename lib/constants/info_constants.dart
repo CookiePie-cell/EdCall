09d92a01998342dc65f687cd4f9a320084bb7b2a
Joseph Julio Nicholas <70589063+CookiePie-cell@users.noreply.github.com>
2023-07-13 13:21:40 +0800

    Update info_constants.dart

--- lib/constants/info_constants.dart
+++ lib/constants/info_constants.dart
@@ -1,5 +1,4 @@
 class InfoConstant {
-  static const String MAIN_URL =
-      'https://fathomless-fortress-79357.herokuapp.com';
-  static const String TARGET_EMAIL = 'pkmdoloduo@gmail.com';
+  static const String MAIN_URL = '';
+  static const String TARGET_EMAIL = 'email@target.com';
 }
