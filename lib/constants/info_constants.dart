83e17c7420ee157bb4b0f609a44261155a3286b6
Julio Nicholas <juliojnicolas1@gmail.com>
2023-07-13 13:18:46 +0800

    bug fix

--- lib/constants/info_constants.dart
+++ lib/constants/info_constants.dart
@@ -1,5 +1,5 @@
 class InfoConstant {
   static const String MAIN_URL =
       'https://fathomless-fortress-79357.herokuapp.com';
-  static const String TARGET_EMAIL = 'ryvalonibala026@student.unsrat.ac.id';
+  static const String TARGET_EMAIL = 'pkmdoloduo@gmail.com';
 }
